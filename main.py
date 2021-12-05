7f03f763eccd8fd31eca68543bbf0d950272b32e
Andrew Havis <andrew@andrew-havis.co.uk>
2021-12-05 11:42:56 +0000

    Fixing a bug that was causing the incorrect number of days to be tweeted.

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from time import sleep
 
 if __name__ == "__main__":
     # Send a tweet every day at 12:00 London time.
-    app = countdown.XmasCountdownBotApp()
     time_format = "%H:%M"
     while True:
         if datetime.datetime.now(tz=timezone("Europe/London")).strftime(time_format) == "12:00":
-            app.tweet()
+            countdown.XmasCountdownBotApp().tweet()
             sleep(60)
