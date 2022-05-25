902971f56727a61767bd363c60c79da95d5a12b2
Andrew Havis <andrew@andrew-havis.co.uk>
2022-05-25 23:26:17 +0100

    Removing the wait until 12 noon as this can be done using Cron.

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import datetime
-
-from pytz import timezone
 from src import app as countdown
-from time import sleep
 
 if __name__ == "__main__":
-    # Send a tweet every day at 12:00 London time.
-    time_format = "%H:%M"
-    while True:
-        if datetime.datetime.now(tz=timezone("Europe/London")).strftime(time_format) == "12:00":
-            countdown.XmasCountdownBotApp().tweet()
-            sleep(60)
+    countdown.XmasCountdownBotApp().tweet()
